b9798da371f5ab785a35aff6dc71188a5fc70768
Gautier DI FOLCO <gautier.difolco@gmail.com>
2022-07-16 17:07:09 +0200

    [sized] Complete implementation

--- sized-wrapper/src/Data/Sized.hs
+++ sized-wrapper/src/Data/Sized.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-redundant-constraints #-}
 
 -- |
 -- Module        : Data.Sized
@@ -15,6 +17,12 @@ module Data.Sized
     Sized,
     getSized,
     trustedSized,
+    unknownSized,
+    Unknown,
+    Between,
+    Exactly,
+    AtLeast,
+    AtMost,
 
     -- * Singleton constructor
     SizedSingleton (..),
@@ -22,13 +30,11 @@ module Data.Sized
     MkSizedSingletonApplicative (..),
 
     -- * From container
+    Size (..),
     SizedFromContainer (..),
-    nonEmpty,
     MkSizedFromContainerFoldable (..),
 
     -- * Operations
-    (<|),
-    (|>),
     overSized,
     overSized2,
     overSized3,
@@ -36,87 +42,98 @@ module Data.Sized
     overSized5,
     fmapSized,
     withSized,
+    precise,
+    approximate,
+    (<<>>),
+    withSizedAppend,
   )
 where
 
 import Data.Kind
 import Data.Maybe (fromJust)
 import Data.Proxy
+import GHC.TypeLits
+import Data.Foldable(toList)
+import Data.Ix(inRange)
 
 -- | Sized proofed value.
-newtype Sized a = Sized
+newtype Sized s a = Sized
   { -- | Extract the Sized proven value
     getSized :: a
   }
   deriving stock (Eq, Ord, Show)
 
-instance Semigroup a => Semigroup (Sized a) where
-  Sized x <> Sized y = Sized $ x <> y
+-- * Different kind of sizes
+-- | Unknown/any size
+data Unknown
+
+-- | Exactly 'n'
+data Exactly (n :: Nat)
+
+-- | At least 'n'
+data AtLeast (n :: Nat)
+
+-- | At most 'n'
+data AtMost (n :: Nat)
+
+-- | Between (included) '(n..m)'
+data Between (n :: Nat) (m :: Nat)
 
 -- * Operations
 
--- | Append empty container
-(<|) :: Semigroup a => Sized a -> a -> Sized a
-Sized ne <| n = Sized $ ne <> n
-{-# INLINE (<|) #-}
-
-infixr 6 <|
-
--- | Prepend empty container
-(|>) :: Semigroup a => a -> Sized a -> Sized a
-n |> Sized ne = Sized $ n <> ne
-{-# INLINE (|>) #-}
-
-infixr 6 |>
-
 -- | Wrap and unwrap 'Sized' (unsafe, be sure 'f' is size-conservative)
-overSized :: (a -> b) -> Sized a -> Sized b
+overSized :: (a -> b) -> Sized s a -> Sized s b
 overSized f = trustedSized . f . getSized
 {-# INLINE overSized #-}
 
 -- | Wrap and unwrap 'Sized' (unsafe, be sure 'f' is size-conservative)
-overSized2 :: (a -> b -> c) -> Sized a -> Sized b -> Sized c
+overSized2 :: (a -> b -> c) -> Sized s a -> Sized s b -> Sized s c
 overSized2 f a = trustedSized . f (getSized a) . getSized
 {-# INLINE overSized2 #-}
 
 -- | Wrap and unwrap 'Sized' (unsafe, be sure 'f' is size-conservative)
-overSized3 :: (a -> b -> c -> d) -> Sized a -> Sized b -> Sized c -> Sized d
+overSized3 :: (a -> b -> c -> d) -> Sized s a -> Sized s b -> Sized s c -> Sized s d
 overSized3 f a b = trustedSized . f (getSized a) (getSized b) . getSized
 {-# INLINE overSized3 #-}
 
 -- | Wrap and unwrap 'Sized' (unsafe, be sure 'f' is size-conservative)
-overSized4 :: (a -> b -> c -> d -> e) -> Sized a -> Sized b -> Sized c -> Sized d -> Sized e
+overSized4 :: (a -> b -> c -> d -> e) -> Sized s a -> Sized s b -> Sized s c -> Sized s d -> Sized s e
 overSized4 f a b c = trustedSized . f (getSized a) (getSized b) (getSized c) . getSized
 {-# INLINE overSized4 #-}
 
 -- | Wrap and unwrap 'Sized' (unsafe, be sure 'f' is size-conservative)
-overSized5 :: (a -> b -> c -> d -> e -> f) -> Sized a -> Sized b -> Sized c -> Sized d -> Sized e -> Sized f
+overSized5 :: (a -> b -> c -> d -> e -> f) -> Sized s a -> Sized s b -> Sized s c -> Sized s d -> Sized s e -> Sized s f
 overSized5 f a b c d = trustedSized . f (getSized a) (getSized b) (getSized c) (getSized d) . getSized
 {-# INLINE overSized5 #-}
 
 -- | 'fmap' over a 'Sized' container
-fmapSized :: Functor f => (a -> b) -> Sized (f a) -> Sized (f b)
+fmapSized :: Functor f => (a -> b) -> Sized s (f a) -> Sized s (f b)
 fmapSized f = overSized (fmap f)
 {-# INLINE fmapSized #-}
 
 -- | Apply an unsafe function over empty, which is safe over 'Sized'
-withSized :: (a -> Maybe b) -> Sized a -> b
+withSized :: (a -> Maybe b) -> Sized s a -> b
 withSized f = fromJust . f . getSized
 {-# INLINE withSized #-}
 
 -- | Trusted value
-trustedSized :: a -> Sized a
+trustedSized :: a -> Sized s a
 trustedSized = Sized
 {-# INLINE trustedSized #-}
+
+-- | Unknown value
+unknownSized :: a -> Sized Unknown a
+unknownSized = Sized
+{-# INLINE unknownSized #-}
 
 -- | Singleton constructible value
 class SizedSingleton a where
   type SizedSingletonElement a :: Type
-  nonEmptySingleton :: Proxy a -> SizedSingletonElement a -> a
+  sizedSingleton :: Proxy a -> SizedSingletonElement a -> a
 
 -- | Build a 'Sized' value from a singleton value
-singleton :: SizedSingleton a => Proxy a -> SizedSingletonElement a -> Sized a
-singleton p = trustedSized . nonEmptySingleton p
+singleton :: SizedSingleton a => Proxy a -> SizedSingletonElement a -> Sized (Exactly 1) a
+singleton p = trustedSized . sizedSingleton p
 {-# INLINE singleton #-}
 
 -- | Build 'SizedSingleton' for 'Applicative' defined types
@@ -128,18 +145,44 @@ newtype MkSizedSingletonApplicative a
 
 instance Applicative f => SizedSingleton (f a) where
   type SizedSingletonElement (f a) = a
-  nonEmptySingleton _ = pure
+  sizedSingleton _ = pure
 
 -- * From container
 
--- | Used to attempt conversion from possibly empty to 'Sized'.
+-- | Used to attempt conversion from possibly any size to 'Sized'.
 class SizedFromContainer a where
-  isSized :: a -> Bool
+  calculateSize :: a -> Int
+  isAtLeast :: Int -> a -> Bool
+  isAtLeast n = (>= n) . calculateSize
+  isAtMost :: Int -> a -> Bool
+  isAtMost n = (<= n) . calculateSize
+  isExactly :: Int -> a -> Bool
+  isExactly n = (== n) . calculateSize
+  isBetween :: Int -> Int -> a -> Bool
+  isBetween n m = inRange (n, m) . calculateSize
 
--- | Attempt 'Sized' proof
-nonEmpty :: SizedFromContainer a => a -> Maybe (Sized a)
-nonEmpty x =
-  if isSized x
+-- | Convert a container from possibly any size to 'Sized'.
+class Size s where
+  sized :: SizedFromContainer a => a -> Maybe (Sized s a)
+
+instance Size Unknown where
+  sized = Just . trustedSized
+
+instance KnownNat n => Size (Exactly n) where
+  sized = mkSized $ isExactly $ fromInteger $ natVal $ Proxy @n
+
+instance KnownNat n => Size (AtLeast n) where
+  sized = mkSized $ isAtLeast $ fromInteger $ natVal $ Proxy @n
+
+instance KnownNat n => Size (AtMost n) where
+  sized = mkSized $ isAtMost $ fromInteger $ natVal $ Proxy @n
+
+instance (KnownNat n, KnownNat m, n <= m) => Size (Between n m) where
+  sized = mkSized $ isBetween (fromInteger $ natVal $ Proxy @n) (fromInteger $ natVal $ Proxy @m)
+
+mkSized :: (a -> Bool) -> a -> Maybe (Sized s a)
+mkSized p x =
+  if p x
     then Just $ trustedSized x
     else Nothing
 
@@ -151,4 +194,58 @@ newtype MkSizedFromContainerFoldable a
   = MkSizedFromContainerFoldable a
 
 instance Foldable f => SizedFromContainer (f a) where
-  isSized = not . null
+  calculateSize = length
+  isExactly n = (== [n]) . drop n . index
+  isAtLeast n = not . null . drop n . index
+  isAtMost n = null . drop n . toList
+  isBetween n m x =
+    let indexed = index x
+      in  elem n indexed && notElem (succ m) indexed
+
+index :: Foldable f => f a -> [Int]
+index = (0:) . zipWith const [1..] . toList
+
+-- | Give a more precise sizing
+precise :: (SizedFromContainer a, Size s) => Sized s' a -> Maybe (Sized s a)
+precise = sized . getSized
+{-# INLINE precise #-}
+
+type family IsMoreGeneral general restrictive :: Constraint where
+  IsMoreGeneral (AtLeast n) (AtLeast m) = (n <= m)
+  IsMoreGeneral (AtMost n) (AtMost m) = (m <= n)
+  IsMoreGeneral (AtLeast n) (Exactly m) = (n <= m)
+  IsMoreGeneral (AtMost n) (Exactly m) = (m <= n)
+  IsMoreGeneral (AtLeast n) (Between m o) = (n <= m)
+  IsMoreGeneral (AtMost n) (Between m o) = (o <= n)
+  IsMoreGeneral (Between n m) (Between o p) = (n <= o, p <= m)
+  IsMoreGeneral (Between n m) (Exactly o) = (n <= o, o <= m)
+  IsMoreGeneral Unknown a = ()
+
+-- | Give a more general sizing
+approximate :: IsMoreGeneral s s' => Sized s' a -> Sized s a
+approximate = trustedSized . getSized
+{-# INLINE approximate #-}
+
+-- | Concatenative operation of 'Sized'
+withSizedAppend :: (a -> b -> c) -> Sized s a -> Sized s' b -> Sized (s <+> s') c
+withSizedAppend f x y = trustedSized $ f (getSized x) (getSized y)
+
+type family (<+>) a b where
+  (<+>) Unknown b = Unknown
+  (<+>) a Unknown = Unknown
+  (<+>) (AtMost n) (AtMost m) = AtMost (n + m)
+  (<+>) (AtLeast n) (AtLeast m) = AtLeast (n + m)
+  (<+>) (AtLeast n) (AtMost m) = Between n (n + m)
+  (<+>) (AtMost n) (AtLeast m) = Between m (n + m)
+  (<+>) (AtLeast n) (Between m o) = Between (n + m) (n + o)
+  (<+>) (AtMost n) (Between m o) = Between (n + m) (n + o)
+  (<+>) (Between m o) (AtLeast n) = Between (n + m) (n + o)
+  (<+>) (Between m o) (AtMost n) = Between (n + m) (n + o)
+  (<+>) (Between n m) (Between o p) = Between (n + m) (o + p)
+
+-- | Semigroup append
+(<<>>) :: Semigroup a => Sized s a -> Sized s' a -> Sized (s <+> s') a
+(<<>>) = withSizedAppend (<>)
+{-# INLINE (<<>>) #-}
+
+infixr 6 <<>>
--- sized-wrapper/test/Data/SizedSpec.hs
+++ sized-wrapper/test/Data/SizedSpec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE NoOverloadedLists #-}
+{-# LANGUAGE NoOverloadedStrings #-}
 module Data.SizedSpec
   ( main,
     spec,
@@ -15,11 +17,81 @@ spec :: Spec
 spec = do
   it "Singleton creation should be equivalent to a single element list" $
     singleton (Proxy @[Int]) 42 `shouldBe` trustedSized [42]
-  it "nonEmpty creation on filled list should be equivalent to the wrapped list" $
-    nonEmpty @[Int] [1, 2, 3] `shouldBe` Just (trustedSized [1, 2, 3])
-  it "nonEmpty creation on empty list should be Nothing" $
-    nonEmpty @[Int] [] `shouldBe` Nothing
-  it "prepend should use semigroup" $
-    singleton (Proxy @[Int]) 1 <| [2] `shouldBe` trustedSized [1, 2]
-  it "append should use semigroup" $
-    [1] |> singleton (Proxy @[Int]) 2 `shouldBe` trustedSized [1, 2]
+  describe "sized creation" $ do
+    describe "Exactly" $ do
+      it "(Exactly 3) on three items list should be equivalent to the wrapped list" $
+        sized @(Exactly 3) @[Int] [1, 2, 3] `shouldBe` Just (trustedSized [1, 2, 3])
+      it "(Exactly 0) on empty list should be equivalent to the wrapped list" $
+        sized @(Exactly 0) @[Int] [] `shouldBe` Just (trustedSized [])
+      it "(Exactly 3) on empty list should be Nothing" $
+        sized @(Exactly 3) @[Int] [] `shouldBe` Nothing
+    describe "AtLeast" $ do
+      it "(AtLeast 3) on three items list should be equivalent to the wrapped list" $
+        sized @(AtLeast 3) @[Int] [1, 2, 3] `shouldBe` Just (trustedSized [1, 2, 3])
+      it "(AtLeast 0) on empty list should be equivalent to the wrapped list" $
+        sized @(AtLeast 0) @[Int] [] `shouldBe` Just (trustedSized [])
+      it "(AtLeast 3) on empty list should be Nothing" $
+        sized @(AtLeast 3) @[Int] [] `shouldBe` Nothing
+    describe "AtMost" $ do
+      it "(AtMost 3) on three items list should be equivalent to the wrapped list" $
+        sized @(AtMost 3) @[Int] [1, 2, 3] `shouldBe` Just (trustedSized [1, 2, 3])
+      it "(AtMost 0) on empty list should be equivalent to the wrapped list" $
+        sized @(AtMost 0) @[Int] [] `shouldBe` Just (trustedSized [])
+      it "(AtMost 3) on a 4-items list should be Nothing" $
+        sized @(AtMost 3) @[Int] [1,2,3,4] `shouldBe` Nothing
+    describe "Between" $ do
+      it "(Between 3 3) on three items list should be equivalent to the wrapped list" $
+        sized @(Between 3 3) @[Int] [1, 2, 3] `shouldBe` Just (trustedSized [1, 2, 3])
+      it "(Between 0 0) on empty list should be equivalent to the wrapped list" $
+        sized @(Between 0 0) @[Int] [] `shouldBe` Just (trustedSized [])
+      it "(Between 3 3) on a 4-items list should be Nothing" $
+        sized @(Between 3 3) @[Int] [1,2,3,4] `shouldBe` Nothing
+      it "(Between 2 4) on three items list should be equivalent to the wrapped list" $
+        sized @(Between 2 4) @[Int] [1, 2, 3] `shouldBe` Just (trustedSized [1, 2, 3])
+      it "(Between 0 4) on empty list should be equivalent to the wrapped list" $
+        sized @(Between 0 4) @[Int] [] `shouldBe` Just (trustedSized [])
+      it "(Between 1 3) on a 4-items list should be Nothing" $
+        sized @(Between 1 3) @[Int] [1,2,3,4] `shouldBe` Nothing
+      it "(Between 5 7) on a 4-items list should be Nothing" $
+        sized @(Between 5 7) @[Int] [1,2,3,4] `shouldBe` Nothing
+  describe "approximate" $ do
+    describe "Exactly" $ do
+      it "(Exactly 3) approximates to (Between 3 3)" $
+        approximate (trustedSized @[Int] @(Exactly 3) [1,2,3]) `shouldBe` trustedSized @[Int] @(Between 3 3) [1,2,3]
+      it "(Exactly 3) approximates to (Between 0 3)" $
+        approximate (trustedSized @[Int] @(Exactly 3) [1,2,3]) `shouldBe` trustedSized @[Int] @(Between 0 3) [1,2,3]
+      it "(Exactly 3) approximates to (Between 3 5)" $
+        approximate (trustedSized @[Int] @(Exactly 3) [1,2,3]) `shouldBe` trustedSized @[Int] @(Between 3 5) [1,2,3]
+      it "(Exactly 3) approximates to (AtLeast 3)" $
+        approximate (trustedSized @[Int] @(Exactly 3) [1,2,3]) `shouldBe` trustedSized @[Int] @(AtLeast 3) [1,2,3]
+      it "(Exactly 3) approximates to (AtMost 3)" $
+        approximate (trustedSized @[Int] @(Exactly 3) [1,2,3]) `shouldBe` trustedSized @[Int] @(AtMost 3) [1,2,3]
+      it "(Exactly 3) approximates to (AtLeast 1)" $
+        approximate (trustedSized @[Int] @(Exactly 3) [1,2,3]) `shouldBe` trustedSized @[Int] @(AtLeast 1) [1,2,3]
+      it "(Exactly 3) approximates to (AtMost 5)" $
+        approximate (trustedSized @[Int] @(Exactly 3) [1,2,3]) `shouldBe` trustedSized @[Int] @(AtMost 5) [1,2,3]
+    describe "AtLeast" $ do
+      it "(AtLeast 3) approximates to (AtLeast 2)" $
+        approximate (trustedSized @[Int] @(AtLeast 3) [1,2,3]) `shouldBe` trustedSized @[Int] @(AtLeast 2) [1,2,3]
+    describe "AtMost" $ do
+      it "(AtMost 3) approximates to (AtMost 4)" $
+        approximate (trustedSized @[Int] @(AtMost 3) [1,2,3]) `shouldBe` trustedSized @[Int] @(AtMost 4) [1,2,3]
+    describe "Between" $ do
+      it "(Between 3 3) approximates to (Between 3 3)" $
+        approximate (trustedSized @[Int] @(Between 3 3) [1,2,3]) `shouldBe` trustedSized @[Int] @(Between 3 3) [1,2,3]
+      it "(Between 3 3) approximates to (Between 0 3)" $
+        approximate (trustedSized @[Int] @(Between 3 3) [1,2,3]) `shouldBe` trustedSized @[Int] @(Between 0 3) [1,2,3]
+      it "(Between 3 3) approximates to (Between 3 5)" $
+        approximate (trustedSized @[Int] @(Between 3 3) [1,2,3]) `shouldBe` trustedSized @[Int] @(Between 3 5) [1,2,3]
+      it "(Between 3 3) approximates to (AtLeast 3)" $
+        approximate (trustedSized @[Int] @(Between 3 3) [1,2,3]) `shouldBe` trustedSized @[Int] @(AtLeast 3) [1,2,3]
+      it "(Between 3 3) approximates to (AtMost 3)" $
+        approximate (trustedSized @[Int] @(Between 3 3) [1,2,3]) `shouldBe` trustedSized @[Int] @(AtMost 3) [1,2,3]
+      it "(Between 3 3) approximates to (AtLeast 1)" $
+        approximate (trustedSized @[Int] @(Between 3 3) [1,2,3]) `shouldBe` trustedSized @[Int] @(AtLeast 1) [1,2,3]
+      it "(Between 3 3) approximates to (AtMost 5)" $
+        approximate (trustedSized @[Int] @(Between 3 3) [1,2,3]) `shouldBe` trustedSized @[Int] @(AtMost 5) [1,2,3]
+  -- it "prepend should use semigroup" $
+  --   singleton (Proxy @[Int]) 1 <| [2] `shouldBe` trustedSized [1, 2]
+  -- it "append should use semigroup" $
+  --   [1] |> singleton (Proxy @[Int]) 2 `shouldBe` trustedSized [1, 2]
